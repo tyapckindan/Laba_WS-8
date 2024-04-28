fd325a9668c83cd5f3fc154d32254a10fc105f2f
Данила Тяпкин <dtapkin6@gmail.com>
2024-04-28 15:05:38 +0300

    Добавьте файлы проекта.

---
+++ Laba №8/Program.cs
@@ -0,0 +1,58 @@
+﻿class Store<T>
+{
+    public T[] AddInfoArr(T[] arr, T info)
+    {
+        T[] temp = new T[arr.Length + 1];
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            temp[i] = arr[i];
+        }
+        temp[temp.Length - 1] = info;
+        return temp;
+    }
+    public T[] DeleteInfoArr(T[] arr, T info)
+    {
+        T[] temp = new T[arr.Length - 1];
+
+        for (int i = 0, k = 0; i < arr.Length; i++)
+        {
+            if (Convert.ToString(arr[i]) != Convert.ToString(info))
+            {
+                temp[k] = arr[i];
+                k++;
+            }
+        }
+        return temp;
+    }
+    public void GetInfoArr(T[] arr, int index) => Console.WriteLine($"{index} элемент массива - это {arr[index - 1]}");
+    public void GetInfoLengthToArr(T[] arr) => Console.WriteLine($"Длина массива: {arr.Length}");
+}
+class PasswordStore: Store<string> 
+{
+    public string[] Passwords = Array.Empty<string>();
+
+    public string[] AddNewPassword(PasswordStore passwords, string password) => passwords.Passwords = passwords.AddInfoArr(passwords.Passwords, password);
+}
+class LoginStore : Store<string>
+{
+    public string[] Logins = Array.Empty<string>();
+
+    public string[] AddNewLogin(LoginStore logins, string login) => logins.Logins = logins.AddInfoArr(logins.Logins, login);
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PasswordStore passwordStore = new PasswordStore();
+        LoginStore loginStore = new LoginStore();
+
+        Console.Write("Добро пожаловать в систему!\nВведите логин: ");
+        string login = Console.ReadLine();
+        loginStore.AddNewLogin(loginStore, login);
+
+        Console.Write("Введите пароль: ");
+        string password = Console.ReadLine();
+        passwordStore.AddNewPassword(passwordStore, password);
+    }
+}
